c254dfbe4d5cc4b6e96dc48087c372bda519eae9
hasler zuniga <haslzun08@hotmail.com>
2019-03-14 22:07:55 -0400

    Initial commit

--- MyInteger.java
+++ MyInteger.java
@@ -1,27 +1,104 @@
-//the purpose of this code is to roll four dices 
-public class Num4 {
-    public static void main(String[] args) {
-        //test object
-        TestNum test  = new TestNum();
-        //calling rolling dice
-        test.rollingDice();
-       
-        
+class MyInteger {
+    private int value;
+
+    public MyInteger(int value) {
+        this.value  = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public boolean isEven() {
+        return (value % 2) == 0;f
+    }
+
+    public boolean isOdd() {
+        return (value % 2) == 1;
+    }
+
+    public boolean isPrime() {
+        if (value == 1 || value == 2) {
+            return true;
+        }
+        else {
+            for (int i = 2; i < value; i++) {
+                if (i % value == 0) return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isEven(int newInt) {
+        return (newInt % 2) == 0;
+    }
+
+    public static boolean isOdd(int newInt) {
+        return (newInt % 2) == 1;
+    }
+
+    public static boolean isPrime(int newInt) {
+        if (newInt == 1 || newInt == 2) {
+            return true;
+        }
+        else {
+            for (int i = 2; i < newInt; i++) {
+                if (i % newInt == 0) return false;
+            }
+        }
+        return true;
+    }    
+
+    public static boolean isEven(MyInteger newInt) {
+        return newInt.isEven();
+    }
+
+    public static boolean isOdd(MyInteger newInt) {
+        return newInt.isOdd();
+    }
+
+    public static boolean isPrime(MyInteger newInt) {
+        return newInt.isPrime();
+    }
+
+    public boolean equals(int testInt) {
+        if (testInt == value) 
+            return true;
+        return false;
+    }
+
+    public boolean equals(MyInteger newInt) {
+        if (newInt.value == this.value) 
+            return true;
+        return false;
+    }
+
+    public static int parseInt(char[] values) {
+        int sum = 0;
+        for (char i : values) {
+            sum += Character.getNumericValue(i);
+        }
+        return sum;
+    }
+
+    public static int parseInt(String value) {
+        return Integer.parseInt(value);
     }
 }
-class TestNum{
-    public void rollingDice(){
-        
-        //for loop to roll the dice 12960
-        for (int i = 1; i <= 12960; i++) {
-            int rand1 = (int)(Math.random() * 6 + 1);
-            int rand2 = (int)(Math.random() * 6 + 1);
-            int rand3 = (int)(Math.random() * 6 + 1);
-            int rand4 = (int)(Math.random() * 6 + 1);
-            int sum = rand1 + rand2 + rand3 + rand4;
-       
-            //prints the sum 
-            System.out.println(sum);
-        }
-    }
+
+public class Exercise1 {
+  public static void main(String[] args) { 
+    MyInteger n1 = new MyInteger(5); 
+    System.out.println("n1 is even? " + n1.isEven()); 
+    System.out.println("n1 is prime? " + n1.isPrime()); 
+    System.out.println("15 is prime? " + MyInteger.isPrime(15));
+    char[] chars = {'3', '5', '3', '9'}; 
+    System.out.println(MyInteger.parseInt(chars)); 
+    String s = "3539"; 
+    System.out.println(MyInteger.parseInt(s)); 
+    MyInteger n2 = new MyInteger(24); 
+    System.out.println("n2 is odd? " + n2.isOdd()); 
+    System.out.println("45 is odd? " + MyInteger.isOdd(45)); 
+    System.out.println("n1 is equal to n2? " + n1.equals(n2)); 
+    System.out.println("n1 is equal to 5? " + n1.equals(5)); }
 }
